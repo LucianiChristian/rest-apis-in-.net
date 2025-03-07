791b5c31a359967a1629b20250134074fe5f86b6
christianluciani <luciani_christian@yahoo.com>
2025-03-06 21:01:53 -0600

    Establish basic CRUD.

---
+++ Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mapping;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
+
+namespace Movies.Api.Controllers;
+
+[ApiController]
+public class MoviesController(IMovieRepository movieRepository) : ControllerBase
+{
+    [HttpGet(ApiEndpoints.Movies.GetAll)]
+    public async Task<IActionResult> GetAll()
+    {
+        var movies = await movieRepository.GetAllAsync();
+
+        var moviesResponse = movies.MapToMoviesResponse();
+
+        return Ok(moviesResponse);
+    }
+
+    [HttpGet(ApiEndpoints.Movies.Get)]
+    public async Task<IActionResult> Get(Guid id)
+    {
+        var movie = await movieRepository.GetByIdAsync(id);
+
+        if (movie is null)
+        {
+            return NotFound();
+        }
+        
+        var movieResponse = movie.MapToMovieResponse();
+
+        return Ok(movieResponse);
+    }
+
+    [HttpPost(ApiEndpoints.Movies.Create)]
+    public async Task<IActionResult> Create(CreateMovieRequest request)
+    {
+        var movie = request.MapToMovie();
+        
+        var createdSuccessfully = await movieRepository.CreateAsync(movie);
+
+        if (!createdSuccessfully)
+        {
+            return BadRequest();
+        }
+        
+        var movieResponse = movie.MapToMovieResponse();
+
+        return CreatedAtAction(nameof(Get), new { id = movie.Id }, movieResponse);
+    }
+
+    [HttpPut(ApiEndpoints.Movies.Update)]
+    public async Task<IActionResult> Update(Guid id, UpdateMovieRequest request)
+    {
+        var updatedMovie = request.MapToMovie(id);
+
+        var updatedSuccessfully = await movieRepository.UpdateAsync(updatedMovie);
+
+        if (!updatedSuccessfully)
+        {
+            return NotFound();
+        }
+
+        return Ok(updatedMovie);
+    }
+
+    [HttpDelete(ApiEndpoints.Movies.Delete)]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var deletedSuccessfully = await movieRepository.DeleteByIdAsync(id);
+
+        if (!deletedSuccessfully)
+        {
+            return NotFound();
+        }
+
+        return Ok();
+    }
+}
--- Movies.Api/Program.cs
+++ Movies.Api/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddControllers();
+
 builder.Services.AddApplicationServices();
 
 var app = builder.Build();
@@ -15,21 +17,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.MapControllers();
 
-// app.MapGet("/weatherforecast", () =>
-//     {
-//         var forecast = Enumerable.Range(1, 5).Select(index =>
-//                 new WeatherForecast
-//                 (
-//                     DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//                     Random.Shared.Next(-20, 55),
-//                     summaries[Random.Shared.Next(summaries.Length)]
-//                 ))
-//             .ToArray();
-//         return forecast;
-//     })
-//     .WithName("GetWeatherForecast")
-//     .WithOpenApi();
+app.UseHttpsRedirection();
 
 app.Run();
